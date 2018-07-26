e132ba072bd69f695081c8028a77595e4471233a
Nipun Manocha <nipun.manocha@1mg.com>
2018-07-26 20:35:23 +0530

    Invoice create and delete

--- app/controllers/api/public/v1/invoices_controller.rb
+++ app/controllers/api/public/v1/invoices_controller.rb
@@ -1,6 +1,6 @@
 module Api::Public::V1
   class InvoicesController < ::Api::Public::AuthController
-    skip_before_action :valid_action?
+    skip_before_action :valid_action?, except: [:create]
 
     def index
       invoices = sales_order.invoices
@@ -11,7 +11,33 @@ module Api::Public::V1
       render_serializer scope: invoice
     end
 
+    def create
+      invoice = sales_order.invoices.create!(param_attributes)
+      render_serializer scope: invoice
+    end
+
+    def destroy
+      invoice.destroy!
+      api_render json: {}
+    end
+
     private
+
+    #
+    # Strong params
+    #
+    def param_attributes
+      params.permit(:number, :date, :attachment)
+    end
+
+    #
+    # Validate create action params
+    #
+    def valid_create?
+      param! :number, String, required: true, blank: false
+      param! :date, Date, required: true, default: Date.today
+      # [TODO nipunmanocha] Validate attachment as file
+    end
 
     #
     # @return [SalesOrder] Sales Order derived from sales_order_id in params
