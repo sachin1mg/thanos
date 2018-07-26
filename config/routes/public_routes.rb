88ce448c1baad2ef2f1d84186b8dcd781b03f8cf
Viren Chugh <viren.chugh@1mg.com>
2018-07-26 17:50:04 +0530

    Created S;2CalesOrderItemsController

--- config/routes/public_routes.rb
+++ config/routes/public_routes.rb
@@ -3,7 +3,9 @@ module PublicRoutes
     router.instance_eval do
       namespace 'api', module: 'api/public' do
         namespace :v1 do
-          resources :sales_orders
+          resources :sales_orders do
+            resources :sales_order_items
+          end
           resources :inventory_pickups
         end
       end
