19f68f52eee0776d99ea98b5c32763f710b22f6e
Rohit Jangid <rohitjangid@outlook.com>
2018-07-26 22:56:05 +0530

    Add more attribtues to location serializer

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,5 +1,10 @@
 class LocationSerializer < ApplicationSerializer
-  attributes :id, :vendor_id, :aisle, :rack, :slab, :bin, :created_at, :updated_at
+  attributes :id, :vendor_id, :aisle, :rack, :slab, :bin,
+             :created_at, :updated_at, :vendor
+
+  def vendor
+    ActiveModelSerializers::SerializableResource.new(object.vendor, fields: [:id, :name]).as_json
+  end
 
   #
   # Default attributes for serializer
