da62a709f28b112dc677d3155b6dac9a0917d740
kamal2793 <kamal.soni2793@gmail.com>
2018-07-26 19:04:03 +0530

    migrations

---
+++ db/migrate/024_create_material_request_items.rb
@@ -0,0 +1,15 @@
+class CreateMaterialRequestItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :material_request_items do |t|
+      t.references :material_request, foreign_key: true
+      t.references :sku, foreign_key: true
+      t.integer :quantity
+      t.integer :status, index: true
+      t.date :schedule_date
+      t.jsonb :metadata
+      t.datetime :deleted_at, index: true
+
+      t.timestamps
+    end
+  end
+end
