54d6e672d5bfbc345d55a7f6eb240a7f6a6b1b50
Sachin Mittal <sachin.mittal@1mg.com>
2018-07-26 12:51:52 +0530

    Change validate method name

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -7,7 +7,7 @@ class Batch < ApplicationRecord
 
   validates_presence_of :manufacturing_date, :expiry_date
   validates_numericality_of :mrp, greater_than: 0
-  validate :manufacturing_expiry_date
+  validate :valid_expiry_date
 
   before_validation :init
 
@@ -15,7 +15,7 @@ class Batch < ApplicationRecord
     self.metadata ||= {}
   end
 
-  def manufacturing_expiry_date
+  def valid_expiry_date
     return if manufacturing_date.nil? || expiry_date.nil?
     if manufacturing_date > expiry_date
       self.errors.add(:expiry_date, 'Should be greater than manufacturing date')
