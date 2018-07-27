0c47af97f66f4f5318bf2e49d3f5f776c253cb03
kamal2793 <kamal.soni2793@gmail.com>
2018-07-27 07:53:57 +0530

    product list page response.

--- app/serializers/inventory_serializer.rb
+++ app/serializers/inventory_serializer.rb
@@ -1,6 +1,14 @@
 class InventorySerializer < ApplicationSerializer
   attributes :id, :vendor_id, :sku_id, :batch_id, :location_id, :quantity,
-    :cost_price, :selling_price, :created_at, :updated_at
+    :cost_price, :selling_price, :created_at, :updated_at, :location, :sku
+
+  def location
+    ActiveModelSerializers::SerializableResource.new(object.location).as_json
+  end
+
+  def sku
+    ActiveModelSerializers::SerializableResource.new(object.sku).as_json
+  end
 
   #
   # Default attributes for serializer
