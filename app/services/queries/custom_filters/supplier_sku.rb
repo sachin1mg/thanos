790fadf451fe1db21e0ab437a711f2949a5b9027
Rohit Jangid <rohitjangid@outlook.com>
2018-07-27 15:26:11 +0530

    Fix filters

--- app/services/queries/custom_filters/supplier_sku.rb
+++ app/services/queries/custom_filters/supplier_sku.rb
@@ -1,11 +1,11 @@
 module Queries::CustomFilters
   class SupplierSku < ::Queries::Filters
     def sku_name(sku_name)
-      scope.joins(:sku).where("sku_name like ?", "%#{sku_name}%")
+      scope.joins(:sku).where("skus.sku_name like ?", "%#{sku_name}%")
     end
 
     def supplier_name(name)
-      scope.joins(:supplier).where("name like ?", "%#{name}%")
+      scope.joins(:supplier).where("suppliers.name like ?", "%#{name}%")
     end
   end
 end
--- app/services/queries/custom_filters/vendor_supplier_scheme.rb
+++ app/services/queries/custom_filters/vendor_supplier_scheme.rb
@@ -5,7 +5,7 @@ module Queries::CustomFilters
     end
 
     def sku_name(sku_name)
-      scope.joins(:sku).where("sku_name like ?", "%#{sku_name}%")
+      scope.joins(:sku).where("skus.sku_name like ?", "%#{sku_name}%")
     end
 
     def supplier_name(supplier_name)
@@ -13,7 +13,7 @@ module Queries::CustomFilters
     end
 
     def scheme_name(scheme_name)
-      scope.joins(:scheme).where("name like ?", "%#{scheme_name}%")
+      scope.joins(:scheme).where("schemes.name like ?", "%#{scheme_name}%")
     end
   end
 end
