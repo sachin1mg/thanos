7dce274ff27ce70b8d7cc34bb9924d37afd03f65
Anubhav Singh <anubhavsingh@1MG-LPTP-363.local>
2018-08-06 16:55:02 +0530

    Added the chec for greater than and equal to 0

--- app/models/adjustment.rb
+++ app/models/adjustment.rb
@@ -5,7 +5,7 @@ class Adjustment < ApplicationRecord
   belongs_to :inventory
 
   validates_presence_of :reason
-  validates_numericality_of :quantity_changed, other_than: 0
+  validates_numericality_of :quantity_changed, greater_than_or_equal_to: 0
 
   before_validation :init
 
