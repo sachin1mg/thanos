2e3a17680f9e5853b836a27c25e364c7dfc5430f
Viren Chugh <viren.chugh@1mg.com>
2018-08-07 16:19:07 +0530

    Made model and migration changes for new mr flow

--- db/migrate/023_create_material_requests.rb
+++ db/migrate/023_create_material_requests.rb
@@ -1,11 +1,14 @@
 class CreateMaterialRequests < ActiveRecord::Migration[5.1]
   def change
     create_table :material_requests do |t|
-      t.references :sales_order, foreign_key: true
+      t.references :user, foreign_key: true
       t.references :vendor, foreign_key: true
+      t.references :sku, foreign_key: true
+      t.integer :sales_order_item_ids, array: true
+      t.integer :quantity
       t.citext :code, index: true
-      t.citext :type, index: true
       t.citext :status, index: true
+      t.date :schedule_date
       t.date :delivery_date
       t.jsonb :metadata
       t.datetime :deleted_at, index: true
